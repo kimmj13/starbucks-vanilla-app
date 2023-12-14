6026ac4980184212d73e130036ba6617d91268d1
kimmj13 <110887976+kimmj13@users.noreply.github.com>
2023-12-14 18:14:15 +0900

    style: promotion 스타일 추가

--- js/main.js
+++ js/main.js
@@ -42,3 +42,53 @@ window.addEventListener(
     }
   }, 300)
 );
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
+
+// new Swiper (선택자, 옵션)
+new Swiper(".notice-line .swiper", {
+  direction: "vertical",
+  autoplay: true,
+  loop: true,
+});
+
+new Swiper(".promotion .swiper", {
+  slidesPerView: 3, //한번에 보일 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
+
+const promotionEl = document.querySelector(".promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener("click", function () {
+  isHidePromotion = !isHidePromotion;
+
+  if (isHidePromotion) {
+    // 숨김
+    promotionEl.classList.add("hide");
+  } else {
+    // 보임
+    promotionEl.classList.remove("hide");
+  }
+});
